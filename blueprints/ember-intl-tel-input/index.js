53b832b94453cd51743434c649bed6fddf038f4e
GabKlein <gabriel.klein.fr@gmail.com>
2017-03-25 10:27:29 +0000

    Fixed blue print

--- blueprints/ember-intl-tel-input/index.js
+++ blueprints/ember-intl-tel-input/index.js
@@ -2,9 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject({
-      [{ name: 'intl-tel-input', target: '9.2.0' }],
-      blueprintOptions: { saveDev: true }
-    });
+    return this.addBowerPackageToProject('intl-tel-input', '9.2.0')
   }
 };
