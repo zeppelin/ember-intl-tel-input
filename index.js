195900a0f5fa4869847712d4ee079226332dc04d
Gabor Babicz <gabor.babicz@gmail.com>
2017-08-09 19:20:13 +0200

    Guard `app.import`s against Fastboot

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* jshint node: true */
 'use strict';
 
+const path = require('path');
+const map = require('broccoli-stew').map;
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-intl-tel-input',
 
@@ -10,11 +15,28 @@ module.exports = {
     var config = app.options.intlTelInput;
 
     if (config && true === config.includeUtilsScript) {
-      app.import(app.bowerDirectory + '/intl-tel-input/build/js/utils.js');
+      app.import('vendor/intl-tel-input/utils.js');
     }
 
-    app.import(app.bowerDirectory + '/intl-tel-input/build/js/intlTelInput.js');
+    app.import('vendor/intl-tel-input/intlTelInput.js');
+
     app.import(app.bowerDirectory + '/intl-tel-input/build/img/flags.png', { destDir: 'assets/images' });
     app.import(app.bowerDirectory + '/intl-tel-input/build/img/flags@2x.png', { destDir: 'assets/images' });
   },
+
+  treeForVendor(defaultTree) {
+    let bowerDirectory = path.resolve('bower_components');
+    let browserVendorLib = new Funnel(bowerDirectory + '/intl-tel-input/build/js', {
+      include: ['*.js'],
+      destDir: 'intl-tel-input'
+    });
+
+    // Wrap each imported library inside a guard against Fastboot
+    // http://ember-fastboot.com/docs/addon-author-guide#third-party-dependencies
+    browserVendorLib = map(browserVendorLib, (content) => {
+      return `if (typeof FastBoot === 'undefined') { ${content} }`
+    });
+
+    return new mergeTrees([defaultTree, browserVendorLib]);
+  }
 };
